cdbc83b5edced6c9229b7782857e0f105390174a
Nathan Belisle <nbelisle@hubspot.com>
2022-08-15 19:02:28 -0400

    Add dot_finicky.js

---
+++ dot_finicky.js
@@ -0,0 +1,39 @@
+// ~/.finicky.js
+
+module.exports = {
+  defaultBrowser: "Brave Browser",
+  rewrite: [
+    {
+      // Redirect all urls to use https
+      match: ({ url }) => url.protocol === "http",
+      url: { protocol: "https" }
+    }
+  ],
+  handlers: [
+    {
+      match: [
+        "*.fidelity.com/*", // match fidelity.com subdomains
+        "*.schwab.com/*",
+      ],
+      browser: "Brave Browser"
+    },
+    {
+      match: [
+        "*.hubteam.com/*", // hubtools
+        "*.hubteamqa.com/*", // hubtools
+        "*.hubspot.com/*", // hubspot
+        "*.hubspotqa.com/*", // hubspot
+        "*.hubspotcentral.com/*", // JIRA
+        "*.hubspotcentral.net/*", // VPN
+        "*.okta.com/*", // OKTA
+        "*.google.com/*", // GSuite (hopefully)
+        "*.amazonaws.com/*", // AWS
+        "*.monday.com/*", // form survey thing
+        "*.dropbox.com/*", // form survey thing
+        "*.hsforms.com/*", // forms
+
+      ],
+      browser: "Google Chrome"
+    }
+  ]
+};
