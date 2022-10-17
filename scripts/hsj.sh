8a88eececf77719468267321c032df5500681cc4
Nathan Belisle <nbelisle@hubspot.com>
2022-10-16 23:10:03 -0400

    Add new expect scripts for hsenv interaction

---
+++ scripts/hsj.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+
+set JANUS_PASS [exec security find-generic-password -s Janus -w]
+
+spawn hsenv -j
+
+expect "Janus username [nbelisle]: "
+
+send -- "\r"
+
+expect "Janus password for nbelisle: "
+
+send -- "$JANUS_PASS\r"
+
+expect "Two-factor authentication required.\r"
+
+send -- "\r"
+
+expect eof
