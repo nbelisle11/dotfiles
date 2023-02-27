e969f58d1e42a194f5601a9b03204552029baf2e
Nate Belisle <nbelisle11@gmail.com>
2023-02-26 23:27:32 -0500

    better comments, newlines at the end of files

--- scripts/gssh.sh
+++ scripts/gssh.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Script to build the scp-like syntax used in git clone when run at the root of an already checked out repository
+
+## Unless otherwise specified, use origin
 if [ "$#" -lt 1 ]; then
     grepvar="origin"
 else
     grepvar=`echo "$@"`
 fi
-echo `git remote -v | grep -e "$grepvar.*\(push\)" | sed -E "s/^$grepvar[[:space:]]+//" | sed 's/[[:space:]]*(push)//'`
+
 # remote | operate on $grepvar / push target | remove "$grepvar  " at the beginning | strip off trailing " (push)"
+echo `git remote -v | grep -e "$grepvar.*\(push\)" | sed -E "s/^$grepvar[[:space:]]+//" | sed 's/[[:space:]]*(push)//'`
