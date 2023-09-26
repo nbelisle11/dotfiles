af2cff7d79f499fdd7f8aa2d2f1e99c80f8708ca
Nate Belisle <nbelisle11@gmail.com>
2023-09-25 23:38:48 -0400

    Update .finicky.js

--- dot_finicky.js
+++ dot_finicky.js
@@ -11,27 +11,7 @@ module.exports = {
     {
       // Use chrome for work specific stuff
       match: [
-        "*dutchie.atlassian.net/*", // Atlassian
-        "*dutchie.zoom.us/*", // Zoom
-        "*dutchie.com/*", // Dutchie
-        "*dutchie.dev/*", // Dutchie
-        "*getdutchie.slack.com/*", // Dutchie Slack links
-        "*github.com/GetDutchie/*", // Github
-        "*paradigmreach.com/*", // paradigm
-        "*google.com/calendar/*", // Calendar links
-        "*docs.google.com/*", // Google office links 
-        "*docker.com/*", // Docker links
-        "*microsoftonline.com/*", // Azure
-        "*microsoft.com/*", // Azure
-        "*microsoft.net/*", // Azure
-        "*windows.net/*", // Azure
         "*1password.com/*", // 1password
-        "*dutchie.cloudflareaccess.com/*", // cloudflare
-        "*dutchie.splunkcloud.com/*", // Splunk
-        "*.okta.com/*", // OKTA
-        "*lucid.app/*", // Lucid suite
-        "*.loom.com/*", // Loom
-        "*.asana.com/*", // Asana
         "*go/*", // golinks
       ],
       browser: "Google Chrome"
