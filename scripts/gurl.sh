9ae340b7afc78719094cfc60caea3447072f4941
Nate Belisle <nate.belisle@datadoghq.com>
2023-10-02 15:49:31 -0400

    split git url parsing into additional step

--- scripts/gurl.sh
+++ scripts/gurl.sh
@@ -12,5 +12,5 @@ else
     grepvar=`echo "$@"`
 fi
 
-echo `git remote -v | grep -e "$grepvar.*\(push\)" | sed -E "s/^$grepvar[[:space:]]+//" | perl -pe 's/(?<!https):/\//' | sed 's/.*git\@/https:\/\//' | sed 's/\.git (push)//'`
-# remote | operate on $grepvar / push target | remove "$grepvar  " at the beginning | strip out ":" only if not preceded by "https" | replace "git@" with "https://" | strip off trailing ".git (push)"
+echo `git remote -v | grep -e "$grepvar.*\(push\)" | sed -E "s/^$grepvar[[:space:]]+//" | perl -pe 's/(?<!https):/\//' | sed 's/.*git\@/https:\/\//' | sed 's/\.git//' | sed 's/ (push)//'`
+# remote | operate on $grepvar / push target | remove "$grepvar  " at the beginning | strip out ":" only if not preceded by "https" | replace "git@" with "https://" | strip off trailing ".git" | strip off trailing " (push)"
