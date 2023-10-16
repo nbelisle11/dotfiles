019a3eeac0787cd8a136dbcc57b4b521a864793e
Nate Belisle <nate.belisle@datadoghq.com>
2023-10-16 17:27:08 -0400

    Update gcm

--- scripts/gcm.sh
+++ scripts/gcm.sh
@@ -7,15 +7,5 @@
 shopt -s expand_aliases
 source ~/.bashrc
 
-localPATH=`pwd`                          # path of current directory
-sep='---------------'
-for d in */; do                          # loop through all directories in the current directory
-  echo $sep"Checking out main branch"$sep
-  mainBranchName=`gemb`                  # use "gemb" aliased script to get the name of the main branch
-  d=`echo $d | sed s#/##`                # remove trailing forward slash from directory name
-  cd $localPATH/$d                       # change directory to current directory being processed
-  git checkout $mainBranchName           # switch to the main branch in the current directory
-  git fetch origin                       # fetch any changes from the remote repository
-  git pull origin $mainBranchName        # pull changes from the remote main branch into the local main branch
-  echo -e '\n'                           # add a blank line for readability
-done
+mainBranchName=`gemb`                  # use "gemb" aliased script to get the name of the main branch
+git checkout $mainBranchName           # switch to the main branch in the current directory
