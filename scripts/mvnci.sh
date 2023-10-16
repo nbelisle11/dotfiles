a9fabc88313bca2bd6e52c697aed17d1b6d2980a
Nate Belisle <nate.belisle@datadoghq.com>
2023-10-16 14:22:31 -0400

    New alias for building specific modules

---
+++ scripts/mvnci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Let me call aliases
+shopt -s expand_aliases
+source ~/.bashrc
+
+# Check if there is at least one parameter given, otherwise exit the script
+if [ "$#" -lt 1 ]; then
+    echo "Illegal number of parameters, must be greater than 1"
+    exit -1
+fi
+
+mvn clean install -X -DskipTests -Dverification.skip -pl $@ -am -amd
